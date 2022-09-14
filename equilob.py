e4615e7c4262aa733c031a23523586316b930719
neutrilo <81818356+neutrilo@users.noreply.github.com>
2022-09-14 20:04:02 -0300

    Continued writting getdata method
    Also convert data extracted from the .CDF files to numpy arrays before saving it into the object.

--- equilob.py
+++ equilob.py
@@ -43,6 +43,9 @@ class eq:
             pass
         class disch:
             pass
+            class qmin:
+                pass
+        
     
         
 
@@ -123,25 +126,76 @@ class eq:
       self.target.seed   = 'eig';   ##
       ### ===================================================
       
-      def getdata(self,file):
-          lablist=["150320W04","154692W02","154358W03","154359W02","147394S01","147634S01","155543W09"]
-          if type(file) == int & len(file) in range(0,7):
-              file=lablist(file)
-          elif type(file) == int & len(file) not in range(0,7):
-              print('If you are typing an int input, it mush be between 0 and 6')
-          elif type(file) == str & file not in lablist:
-              print('If you are typing an string as input, it must be in the next list:')
-              print('Valid data ID list: ', lablist)
-          fname=file+'.cdf'
-          dataobj=nc.Dataset(fname,'r',format="NETCDF4")
-          #Time
-          self.ptransp.disch.t    = dataobj['/TIME']        # (s)
-              
-              
+    def getdata(self,file,time_slice='Mean'):
+        import numpy as np
+        import netCDF4 as nc
+        lablist=["150320W04","154692W02","154358W03","154359W02","147394S01","147634S01","155543W09"]
+        if (type(file) == int) & (file in range(7)):
+            file=lablist[file]
+        elif (type(file) == int) & (file not in range(7)):
+           print('If you are typing an int type input, it must be between 0 and 6')
+        elif (type(file) == str) & (file not in lablist):
+           print('If you are typing an string as input, it must be in the next list:')
+           print('Valid data ID list: ', lablist)
+        elif (type(file) != str) & (type(file) == int):
+            print('Input must be an int or str.')
+          
+        fname=file+'.CDF'
+        dataobj=nc.Dataset(fname,'r',format="NETCDF4")
+        #Time
+        self.ptransp.disch.t    = np.array(dataobj['TIME'])        # (s)
+        nit = len(np.array(dataobj['/TIME']))
+
+        ## Evolution of some variables
+        self.ptransp.disch.Ip   = np.array(dataobj['PCUR'])*1.e-6  # (MA)
+        self.ptransp.disch.q0   = np.array(dataobj['Q0'])
+        self.ptransp.disch.pec1 = np.array(dataobj['PECIN1'])
+        self.ptransp.disch.pec2 = np.array(dataobj['PECIN2'])
+        self.ptransp.disch.pec3 = np.array(dataobj['PECIN3'])
+        self.ptransp.disch.pec4 = np.array(dataobj['PECIN4'])
+        self.ptransp.disch.pec5 = np.array(dataobj['PECIN5'])
+        self.ptransp.disch.pec6 = np.array(dataobj['PECIN6'])
+        self.ptransp.disch.pnb1 = np.array(dataobj['PINJ01'])
+        self.ptransp.disch.pnb2 = np.array(dataobj['PINJ02'])
+        self.ptransp.disch.pnb3 = np.array(dataobj['PINJ03'])
+        self.ptransp.disch.pnb4 = np.array(dataobj['PINJ04'])
+        self.ptransp.disch.pnb5 = np.array(dataobj['PINJ05'])
+        self.ptransp.disch.pnb6 = np.array(dataobj['PINJ06'])
+        self.ptransp.disch.pnb7 = np.array(dataobj['PINJ07'])
+        self.ptransp.disch.pnb8 = np.array(dataobj['PINJ08'])
+        self.ptransp.disch.pnb9 = np.array(dataobj['PINJ09'])
+        self.ptransp.disch.pnb10 = np.array(dataobj['PINJ10'])
+        self.ptransp.disch.pnb11 = np.array(dataobj['PINJ11'])
+        self.ptransp.disch.pnb12 = np.array(dataobj['PINJ12'])
+        self.ptransp.disch.pnb13 = np.array(dataobj['PINJ13'])
+        self.ptransp.disch.pnb14 = np.array(dataobj['PINJ14'])
+        #print(nit)
+        #print(np.shape(np.array(dataobj['Q'])))
+        self.ptransp.disch.qmin = np.zeros(nit)
+        for iq in range(nit):
+           self.ptransp.disch.qmin[iq] = np.array(dataobj['Q'])[iq,:].min()
+           
+        
+        self.ptransp.disch.beta  = np.array(dataobj['BETAT'])/100
+        self.ptransp.disch.p0    = np.array(dataobj['PMHD_IN'][1,:])  # (Pa)
+        self.ptransp.disch.Raxis = np.array(dataobj['RAXIS'])/100  # (m)
+        self.ptransp.disch.B0    = np.array(dataobj['GB1'][1,:])      # (T)
+        self.ptransp.disch.Baxis = np.array(dataobj['BZXR'])/np.array(dataobj['RAXIS'])
+        self.ptransp.disch.Psi0  = np.array(dataobj['PLFLXA'])
+        self.ptransp.disch.Psi0D = np.array(dataobj['PLFLXD'])
+        
+        if time_slice == 'Mean':
+            time_slice = np.mean(self.ptransp.disch.t)
+        elif type(time_slice) == str:
+            print('time_slice variable must be a number or just nothing')
+        time_slice=max(time_slice,min(self.ptransp.disch.t))
+        time_slice=min(time_slice,max(self.ptransp.disch.t))
+        
                 
 
         
 ob=eq('dif_01')
+ob.getdata(0)
 #equ=selfibrium()
 #self.numpar.solver.Nmeshsize = 300
 
